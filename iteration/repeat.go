7f4697b57f647f232ef06825b5f597e85b859734
pwinning1991 <ftcpjw@yahoo.com>
2019-08-24 09:58:54 -0400

    updated to take a number of times parameter

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,8 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(s string) string {
+func Repeat(s string, n int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < n; i++ {
 		repeated = repeated + s
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q got %q", expected, repeated)
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
