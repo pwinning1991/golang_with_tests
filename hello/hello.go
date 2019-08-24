8ac67ca36f89d7f7f3cf67b22e040574093e3449
pwinning1991 <ftcpjw@yahoo.com>
2019-08-24 09:22:49 -0400

    abstracted language prefix to another function

--- hello/hello.go
+++ hello/hello.go
@@ -12,14 +12,20 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
