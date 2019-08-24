3e4dc5c20dcc182c3ecbaba3da603b251622702c
pwinning1991 <ftcpjw@yahoo.com>
2019-08-24 16:44:34 -0400

    updated to use custom type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,15 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+type Dictonary map[string]string
+
+func (d Dictonary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
