9dd50ddbd764cd934f04ac8cff926c43e37bbc20
pwinning1991 <ftcpjw@yahoo.com>
2019-08-24 10:34:54 -0400

    updated to use a slice

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,11 +3,14 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+	t.Run("collections of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
