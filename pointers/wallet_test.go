b71c59c23aff72eb348b3a011a9453354f673a5b
pwinning1991 <ftcpjw@yahoo.com>
2019-08-24 15:19:55 -0400

    updted to add test for no error call

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -18,10 +18,11 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.WithDraw(10)
+		err := wallet.WithDraw(10)
 
 		want := Bitcoin(10)
 		assertBalance(t, wallet, want)
+		assertNoError(t, err)
 
 	})
 
@@ -53,5 +54,12 @@ func assertError(t *testing.T, got error, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
