ffdd82e4cd12b28db9f007676731b05d50d823c2
pwinning1991 <ftcpjw@yahoo.com>
2019-08-26 20:34:40 -0400

    refactoed to implement error interface

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,15 @@
 package maps
 
-import "errors"
-
 var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word  because it already exists")
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word  because it already exists")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictonary map[string]string
 
