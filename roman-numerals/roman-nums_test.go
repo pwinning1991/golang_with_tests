0b51e3530f1aa20216b5e701f446232d9eaaa975
pwinning1991 <ftcpjw@yahoo.com>
2019-09-10 23:11:42 -0400

    updated to work for 9

--- roman-numerals/roman-nums_test.go
+++ roman-numerals/roman-nums_test.go
@@ -9,13 +9,14 @@ func TestRomanNumerals(t *testing.T) {
 		Want        string
 	}{
 		{"1 gets contverted to I", 1, "I"},
-		{"2 gets convdrted to II", 2, "II"},
-		{"3 gets convdrted to III", 3, "III"},
-		{"4 gets convdrted to IV (can't repeat more than 3 time))", 4, "IV"},
-		{"5 gets convdrted to V )", 5, "V"},
-		{"6 gets convdrted to VI )", 6, "VI"},
-		{"7 gets convdrted to VII )", 7, "VII"},
-		{"8 gets convdrted to VIII )", 8, "VIII"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 time))", 4, "IV"},
+		{"5 gets converted to V )", 5, "V"},
+		{"6 gets converted to VI )", 6, "VI"},
+		{"7 gets converted to VII )", 7, "VII"},
+		{"8 gets converted to VIII )", 8, "VIII"},
+		{"9 gets converted to IX)", 9, "IX"},
 	}
 
 	for _, test := range cases {
