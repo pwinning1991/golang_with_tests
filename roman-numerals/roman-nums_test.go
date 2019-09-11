c9ed50b462a1d1e11f9a1c9288389f61fd8fb65f
pwinning1991 <ftcpjw@yahoo.com>
2019-09-10 22:32:07 -0400

    refactored test code

--- roman-numerals/roman-nums_test.go
+++ roman-numerals/roman-nums_test.go
@@ -3,21 +3,21 @@ package roman
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets contverted to I", 1, "I"},
+		{"2 gets convdrted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("2 gets converetd to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
+	}
 }
