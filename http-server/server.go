62c4898bfddbc8aaced649d08c8691300011f8b6
pwinning1991 <ftcpjw@yahoo.com>
2019-09-23 23:34:01 -0400

    added types and interfaces for tests and in code

--- http-server/server.go
+++ http-server/server.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 
-	fmt.Fprint(w, GetPlayerScore(player))
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 
 }
 
