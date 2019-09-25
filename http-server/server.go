bc8de6f31e98ab101fcfd60dd46865c8e3765f12
pwinning1991 <ftcpjw@yahoo.com>
2019-09-24 20:51:49 -0400

    added post method to store wincalls

--- http-server/server.go
+++ http-server/server.go
@@ -7,6 +7,7 @@ import (
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 type PlayerServer struct {
@@ -14,7 +15,18 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	player := r.URL.Path[len("/players/"):]
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 
 	score := p.store.GetPlayerScore(player)
 
@@ -23,7 +35,11 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+}
 
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func GetPlayerScore(name string) string {
