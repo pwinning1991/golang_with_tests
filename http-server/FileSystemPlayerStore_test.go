43b7a4b4f5949ec516b0c4dd8a2c9638013e0217
Philip Winnington <ftcpjw@yahoo.com>
2019-09-25 22:37:24 -0400

    adding getplayer score function

--- http-server/FileSystemPlayerStore_test.go
+++ http-server/FileSystemPlayerStore_test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("/league from a reader", func(t *testing.T) {
 
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},	
 			{"Name": "Chris", "Wins": 33} ]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -23,4 +26,48 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},	
+			{"Name": "Chris", "Wins": 33} ]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+		assertScoreEquals(t, got, want)
+
+	})
+}
+
+func createTempFile(t *testing.T, initalData string) (io.ReadWriteSeeker, func()) {
+
+	t.Helper()
+
+	tmpfile, err := ioutil.TempFile("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initalData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
 }
