fc90d37defef55f24fc88f6047ed320da61fa21c
pwinning1991 <ftcpjw@yahoo.com>
2019-09-26 23:09:24 -0400

    added type tape and updated contrcutor to use

--- http-server/FileSystemPlayerStore.go
+++ http-server/FileSystemPlayerStore.go
@@ -6,7 +6,7 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
@@ -33,7 +33,6 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(f.league)
 }
 
@@ -41,7 +40,7 @@ func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStor
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
 	return &FileSystemPlayerStore{
-		database: database,
+		database: &tape{database},
 		league:   league,
 	}
 }
