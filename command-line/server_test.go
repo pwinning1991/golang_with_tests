f7641550ae5b85634b90e7c68e9dc6adff83c8fb
Philip Winnington <ftcpjw@yahoo.com>
2019-09-28 15:49:07 -0400

    refator test code

--- command-line/server_test.go
+++ command-line/server_test.go
@@ -86,14 +86,8 @@ func TestStoreWins(t *testing.T) {
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
+		assertPlayerWin(t, &store, player)
 
-		if len(store.winCalls) != 1 {
-			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
-		}
 	})
 }
 
