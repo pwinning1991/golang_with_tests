b7c4aa870bfcae5751fea080953997297205d85c
pwinning1991 <ftcpjw@yahoo.com>
2019-09-28 10:01:25 -0400

    adding initlaise function to constructor

--- http-server/FileSystemPlayerStore.go
+++ http-server/FileSystemPlayerStore.go
@@ -37,12 +37,37 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	f.database.Encode(f.league)
 }
 
-func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+func initialisePlayerDBFile(file *os.File) error {
 	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info feom %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
+}
+
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+
+	err := initialisePlayerDBFile(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
+	}
+
 	league, err := NewLeague(file)
+
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
+
 	return &FileSystemPlayerStore{
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
