997caf845d409a25f2a7ce80424a7dadf957bd2a
pwinning1991 <ftcpjw@yahoo.com>
2019-12-22 11:16:10 -0500

    test passing for blind scheduling

--- time/CLI_test.go
+++ time/CLI_test.go
@@ -1,6 +1,7 @@
 package poker_test
 
 import (
+	"fmt"
 	"github.com/pwinning1991/golang_with_tests/time"
 	"io"
 	"strings"
@@ -19,8 +20,43 @@ func TestCLI(t *testing.T) {
 
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
-		if len(blindAlerter.alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		cases := []struct{
+			expectedScheduleTime time.Duration
+			expectedAmount int
+		} {
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled at %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T){
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				amountGot := alert.amount
+				if amountGot != c.expectedAmount {
+					t.Errorf("got amount %d, eant %d", amountGot, c.expectedAmount)
+				}
+
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != c.expectedScheduleTime {
+					t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, c.expectedScheduleTime)
+				}
+
+			})
+
 		}
 
 	})
